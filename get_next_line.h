aa9afa482855aaceafda29440bf8776376f73688
peladno <j.perezurrutia@gmail.com>
2026-05-04 19:12:44 +0900

    linked list struct added

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,28 @@
 /*   By: jperez-u <jperez-u@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 23:01:06 by jperez-u          #+#    #+#             */
-/*   Updated: 2026/05/02 23:10:07 by jperez-u         ###   ########.fr       */
+/*   Updated: 2026/05/04 18:36:23 by jperez-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
 # endif
 
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
 
-char *get_next_line(int fd);
+char				*get_next_line(int fd);
 
 #endif
